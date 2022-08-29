b4e3667cff2cea2b80eea05fbf12a1c2b89afa60
federico_calandra <federico12597@gmial.com>
2022-08-29 16:27:31 +0200

    Added operation_builder.py

--- bin/operations/operation.py
+++ bin/operations/operation.py
@@ -1,10 +1,16 @@
 from bin.nodes.travel_node import TravelNode
+from bin.operations.flight import Flight
 from bin.operations.movement import Movement
+from bin.problem_instantiator import ProblemInstance
 from bin.veicles.truck import Truck
 
 
 class Operation:
-    def __init__(self, start_node: TravelNode, end_node: TravelNode, flights, truck: Truck):
+    def __init__(self, problem_instance: ProblemInstance, start_node: TravelNode, end_node: TravelNode,
+                 flights: list[Flight], truck: Truck):
+        self.problem_instance = problem_instance
+        self.start_node = start_node
+        self.end_node = end_node
         self.flights = flights
         self.truck_movement = Movement(start_node, end_node, truck)
 
@@ -13,3 +19,22 @@ class Operation:
         for flight in self.flights:
             str_flights += str(flight) + "\n"
         return "Truck: " + str(self.truck_movement) + "\nDrone flights:\n" + str_flights
+
+    def is_feasible(self):
+        is_feasible = True
+        if self.flights:
+            for flight in self.flights:
+                if flight.compute_energy_used_for_flight(self.problem_instance) > \
+                        self.problem_instance.drone.max_energy_available:
+                    is_feasible = False
+                    break
+        return is_feasible
+
+    def compute_operation_time(self):
+        drone_times = []
+        for flight in self.flights:
+            drone_times.append(flight.compute_flight_time(self.problem_instance))
+        max_drone_time = max(drone_times)
+        truck_time = self.truck_movement.compute_movement_time(
+            self.problem_instance.compute_distance_for_truck(self.start_node, self.end_node))
+        return max(truck_time, max_drone_time)
---
+++ bin/util/operation_builder.py
@@ -0,0 +1,49 @@
+import math
+from bin.nodes.client_node import ClientNode
+from bin.nodes.node import Node
+from bin.operations.flight import Flight
+from bin.operations.operation import Operation
+from bin.problem_instantiator import ProblemInstance
+
+
+class OperationBuilder:
+    def __init__(self, problem_instance: ProblemInstance, start_node: Node, end_node: Node,
+                 number_of_clients_to_be_served: int, number_of_served_clients: int, visit_order: list[ClientNode]):
+        self.problem_instance = problem_instance
+        self.start_node = start_node
+        self.end_node = end_node
+        self.number_of_clients_to_be_served = number_of_clients_to_be_served
+        self.number_of_served_clients = number_of_served_clients
+        self.visit_order = visit_order.copy()
+
+    def build_operation(self):
+        # noinspection PyTypeChecker
+        operation = Operation(self.problem_instance, self.start_node, self.end_node,
+                              self.compute_flights_in_operation(), self.problem_instance.truck)
+        return operation if operation.is_feasible() else None
+
+    def compute_flights_in_operation(self):
+        drone_assignments_list = self.compute_drone_assignments()
+        flights = []
+        clients_to_be_served = self.visit_order[self.number_of_served_clients : self.number_of_clients_to_be_served]
+        pointer = 0
+        for number_of_clients in drone_assignments_list:
+            visited_clients = clients_to_be_served[pointer : number_of_clients]
+            pointer += number_of_clients
+            flights.append(Flight(self.start_node, self.end_node, visited_clients, self.problem_instance.drone))
+        return flights
+
+    # block assignments
+    def compute_drone_assignments(self):
+        n = self.number_of_clients_to_be_served
+        drone_assignments_list = []
+        while n > 0:
+            clients_visited_by_drone = math.ceil(self.number_of_clients_to_be_served /
+                                                 self.problem_instance.number_of_available_drones)
+            if clients_visited_by_drone < n:
+                drone_assignments_list.append(clients_visited_by_drone)
+                n -= clients_visited_by_drone
+            else:
+                drone_assignments_list.append(n)
+                n = 0
+        return drone_assignments_list
